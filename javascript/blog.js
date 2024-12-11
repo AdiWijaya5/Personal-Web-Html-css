7289a4f7b7abe3a17b1523279248138335cb899d
Adi Wijaya <adiwijaya5699@gmail.com>
2024-12-11 21:03:35 +0700

    update

--- javascript/blog.js
+++ javascript/blog.js
@@ -3,23 +3,28 @@ let blogs = []; // length nya adalah 0
 function addBlog(e) {
   e.preventDefault();
 
-  let title = document.getElementById("input-project-title").value;
-  let content = document.getElementById("input-project-content").value;
-  let imageInput = document.getElementById("input-project-image");
+  let project = document.getElementById('input-project-title').value;
+  let content = document.getElementById('input-project-content').value;
+  let imageInput = document.getElementById('input-project-image');
 
-  if (title == "" || content == "" || imageInput.files.length === 0) {
-    return alert("All input fields cannot be empty");
+  if (project == '' || content == '' || imageInput.files.length === 0) {
+    return alert('All input fields cannot be empty');
   }
 
-  let skills = document.querySelectorAll('input[type="checkbox"]').value;
+  // let arrChechbox = [];
+  // let checkBox = document.querySelectorAll("input[type='checkbox]:checked");
+  // for (let i = 0; i < checkBox.length; i++) {
+  //   arrChechbox.push(checkBox[i].value);
+  // }
+  // let skills = document.querySelectorAll('input[type="checkbox"]').value;
 
-  let dateControl = document.querySelector('input[type="date"]').value;
+  let dateEnd = document.querySelector('input[type="date"]').value;
   let image = URL.createObjectURL(imageInput.files[0]);
 
   let blog = {
-    datestart: dateControl,
-    teknologi: skills,
-    title: title,
+    title: project,
+    datestart: dateEnd,
+    // teknologi: arrChechbox,
     content: content,
     image: image,
     postedAt: new Date(),
@@ -33,13 +38,13 @@ function addBlog(e) {
 function renderBlog() {
   console.log(blogs);
 
-  let blogListElement = document.getElementById("blogList");
+  let blogListElement = document.getElementById('blogList');
 
   blogListElement.innerHTML = firstBlogContent();
 
   for (let i = 0; i < blogs.length; i++) {
     let formattedDate = formatDateToWIB(blogs[i].postedAt);
-    // menampilkan blogs yang sudah kita buat dengan mengisi form
+    // menampilkan my projecct
     console.log(blogs[i]);
 
     blogListElement.innerHTML += `
@@ -54,9 +59,7 @@ function renderBlog() {
                 <a href="blog-detail.html" class="blog-item-title">
                 ${blogs[i].title} - ${blogs[i].datestart}
                 </a>
-                <p class="relative-time">${getRelativeTime(
-                  blogs[i].postedAt
-                )}</p>
+                <p class="relative-time">${getRelativeTime(blogs[i].postedAt)}</p>
               </h1>
               <p class="blog-text">
               ${blogs[i].content}
@@ -69,7 +72,7 @@ function renderBlog() {
             </div>
               <div class="btn-group">
                 <button class="btn-edit">Edit</button>
-                <button class="btn-delet">Delete</button>
+                <button id="1" type="button" class="btn-delet" onclick="remove(this)">Delete</button>
               </div>
             </div>
           </div>
@@ -105,7 +108,7 @@ function firstBlogContent() {
             <div class="blog-content">
               <div class="btn-group">
                 <button class="btn-edit">Edit</button>
-                <button class="btn-delet">Delete</button>
+                 <button id="+countre+" type="button" class="btn-delet" onclick="remove(this)">Delete</button>
               </div>
               </div>
             </div>
@@ -114,28 +117,17 @@ function firstBlogContent() {
     `;
 }
 function formatDateToWIB(date) {
-  let months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "Mei",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Okr",
-    "Nov",
-    "Des",
-  ];
+  let months = ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Aug', 'Sep', 'Okr', 'Nov', 'Des'];
 
-  let day = date.getDate().toString().padStart(2, "0");
+  let day = date.getDate().toString().padStart(2, '0');
+
   let month = months[date.getMonth()];
+
   let year = date.getFullYear();
 
-  let hours = date.getHours().toString().padStart(2, "0");
+  let hours = date.getHours().toString().padStart(2, '0');
 
-  let minutes = date.getMinutes().toString().padStart(2, "0");
+  let minutes = date.getMinutes().toString().padStart(2, '0');
 
   let formattedDate = `${day} ${month} ${year} ${hours}:${minutes} WIB`;
 
@@ -144,19 +136,32 @@ function formatDateToWIB(date) {
 
 function getRelativeTime(targetDate) {
   let now = new Date();
-  let diffInSeconds = Math.floor((now - targetDate) / 1000); 
+  let diffInSeconds = Math.floor((now - targetDate) / 1000);
   console.log(diffInSeconds);
 
   if (diffInSeconds < 60) {
-    return `Durasi : ${diffInSeconds} second${
-      diffInSeconds > 1 ? "s" : ""
-    } ago`;
+    return `Durasi : ${diffInSeconds} second${diffInSeconds > 1 ? 's' : ''} ago`;
   }
 
   let diffInMinutes = Math.floor(diffInSeconds / 60);
   if (diffInMinutes < 60) {
-    return `Durasi : ${diffInMinutes} minute${
-      diffInMinutes > 1 ? "s" : ""
-    } ago`;
+    return `Durasi : ${diffInMinutes} minute${diffInMinutes > 1 ? 's' : ''} ago`;
   }
+  let diffInHours = Math.floor(diffInMinutes / 60);
+  if (diffInHours < 24) {
+    return `${diffInHours} hours${diffInHours > 1 ? 's' : ''}  ago;`;
+  }
+  let diffInDays = Math.floor(diffInHours / 24);
+  if (diffInDays < 30) {
+    return `${diffInDays} day${diffInDays > 1 ? 's' : ''} ago`;
+  }
+
+  let diffInMonth = Math.floor(diffInDays / 30);
+  return `${diffInMonth} monts${diffInMonth > 1 ? 's' : ''} ago`;
 }
+
+// function remove(button) {
+//   let number = button.id;
+//   let blogs = document.getElementById(blogs);
+//   blogs.remove;
+// }
